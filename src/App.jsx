c31fcb33e36249b24f13c08a65d3c5984cbc078a
angel <56078897+dewebsic@users.noreply.github.com>
2022-08-30 11:14:27 -0400

    mejorando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { Container } from "@mui/material";
-import Navbar from "./shared/components/Navbar";
+import ResponsiveDrawer from "./components/ResponsiveDrawer";
 
 export default function App() {
    return (
-     <Container maxWidth="xl">
-        <Navbar/>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto aperiam quod delectus autem, culpa atque odio incidunt dolor quo optio iure dolores deserunt eos officiis nihil aliquid eligendi voluptas nostrum.</h1>
-     </Container>
+     <ResponsiveDrawer/>
    );
 }
